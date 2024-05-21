b82236829eadbebc3c87e5c41e09d7e8cd4d4c4e
Peyton Seigo <pseigo@yahoo.com>
2024-05-20 19:29:13 -0600

    Add Thermostat component

---
+++ src/components/thermostat.svelte
@@ -0,0 +1,29 @@
+<div class="thermostat">
+  <h2>Temperature</h2>
+  <div class="temperature">{temperature}{temperature_unit_short}</div>
+  <button on:click={increment}>+</button>
+  <button on:click={decrement}>-</button>
+</div>
+
+<style>
+  .thermostat > .temperature {
+    font-size: 4em;
+    font-weight: 700;
+  }
+</style>
+
+<script lang="ts">
+  let bounds = { temperature: { min: 0, max: 40} };
+  export let temperature: number = 0;
+  const temperature_step: number = 0.5;
+  let temperature_unit_short = "\u00B0C";
+
+  function increment() {
+    temperature = Math.min(bounds.temperature.max, temperature + temperature_step);
+  }
+
+  function decrement() {
+    temperature = Math.max(bounds.temperature.min, temperature - temperature_step);
+  }
+</script>
+
